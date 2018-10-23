5a7f8bb0e9e142a9784ba7a37ee77d73e06cd81a
ManishM9 <mr.malepati@gmail.com>
2018-10-24 02:09:33 +0530

    AANVIK

---
+++ src/app/components/aanvik/aanvik.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aanvik',
+  templateUrl: './aanvik.component.html',
+  styleUrls: ['./aanvik.component.css']
+})
+export class AanvikComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
